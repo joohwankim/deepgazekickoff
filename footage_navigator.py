cc88af47981ad608f31431fafd61046bf36eb49d
joohwankim <sckim@nvidia.com>
2018-05-23 13:47:28 -0700

    adding 'add' action

--- footage_navigator.py
+++ footage_navigator.py
@@ -4,7 +4,7 @@
 # 3. Provide a clipboard copy of the paths to the selected items.
 
 import json, pdb, glob, os, sys, shutil
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout,QHeaderView, QAbstractItemView, QMenuBar, QAction
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout,QHeaderView, QAbstractItemView, QMenuBar, QAction, QFileDialog, QInputDialog
 from PyQt5 import QtGui
 from PyQt5.QtGui import QClipboard
 # from PyQt5.QtGui import QIcon, QColor, QMenuBar
@@ -13,12 +13,12 @@ from PyQt5.QtCore import pyqtSlot
 class data_manager_backend():
     def update_json(self):
         with open(os.path.join(self.footage_path, self.description_filename),'w') as fw:
-            json.dump(self.descriptions, fw, indent = 4)
+            json.dump(self.active_descriptions, fw, indent = 4)
 
     def get_desc_key_list(self):
         # generate key list of all the items in the descriptions
         keys = set()
-        for desc in self.descriptions:
+        for desc in self.active_descriptions:
             for key in desc.keys():
                 keys.add(key)
         keys = list(keys)
@@ -32,87 +32,93 @@ class data_manager_backend():
         keys.insert(len(keys), keys.pop(keys.index('setname')))
         return list(keys)
 
-    def sort_descriptions(self, sort_key):
-        sorted_descriptions = sorted(self.descriptions, key = lambda x:x[sort_key])
-        self.descriptions = sorted_descriptions
+    def sort_descriptions(self, sort_key): # sort active descriptions
+        sorted_descriptions = sorted(self.active_descriptions, key = lambda x:x[sort_key])
+        self.active_descriptions = sorted_descriptions
 
     def __init__(self, folder_path):
         # import descriptions
         self.footage_path = folder_path
-        self.inactive_files_path = os.path.join(folder_path, 'inactive')
-        self.active_files_path = os.path.join(folder_path, 'active')
-        self.inactive_files_path = os.path.join(folder_path, 'inactive')
-        self.description_filename = "footage_descriptions.json"
-        with open(os.path.join(self.footage_path, self.description_filename),'r') as fr:
+        self.candidate_folder_path = os.path.join(folder_path, 'candidates')
+        self.active_folder_path = os.path.join(folder_path, 'active')
+        self.inactive_folder_path = os.path.join(folder_path, 'inactive')
+        self.description_file_path = os.path.join(folder_path, "descriptions.json")
+        with open(self.description_file_path,'r') as fr:
             json_str = fr.read()
-            self.descriptions = json.loads(json_str)
-        self.sort_descriptions('setname')
+            self.all_descriptions = json.loads(json_str)
+            self.active_descriptions = list()
+            self.inactive_descriptions = list()
+            for desc in self.all_descriptions:
+                if desc['status'] == 'active':
+                    self.active_descriptions.append(desc)
+                elif desc['status'] == 'inactive':
+                    self.inactive_descriptions.append(desc)
         if self.is_any_set_missing(): # make sure all the sets are present in the footage folder.
             sys.exit("Some sets listed in the description file are missing.")
 
     def is_any_set_missing(self):
-        dirs = os.listdir(self.footage_path)
-        missing_dir_count = 0
-        for description in self.descriptions:
-            if not description['setname'] in dirs:
-                missing_dir_count += 1
-        if missing_dir_count > 0:
+        active_sets = os.listdir(self.active_folder_path)
+        inactive_sets = os.listdir(self.inactive_folder_path)
+        missing_set_count = 0
+        for desc in self.active_descriptions:
+            if not desc['setname'] in active_sets:
+                missing_set_count += 1
+        for desc in self.inactive_descriptions:
+            if not desc['setname'] in inactive_sets:
+                missing_set_count += 1
+        if missing_set_count > 0:
             return True
         else:
             return False
 
-    def delete_set(self, set_ids):
+    def delete_sets(self, set_ids):
         # make sure inactive files folder exists and create one if not.
-        if not os.path.exists(self.inactive_files_path):
-            os.mkdir(self.inactive_files_path)
-        for desc in self.descriptions:
+        if not os.path.exists(self.inactive_folder_path):
+            os.mkdir(self.inactive_folder_path)
+        for desc in self.active_descriptions:
             if desc['id'] in set_ids: # this set should be deleted (moved to the 'inactive files' folder).
                 # determine the destination file name
                 dst_setname = ''
-                if os.path.exists(os.path.join(self.inactive_files_path, desc['setname'])): # if folder already exists
+                if os.path.exists(os.path.join(self.inactive_folder_path, desc['setname'])): # if folder already exists
                     try_cnt = 1
                     while(True):
-                        if os.path.exists(os.path.join(self.inactive_files_path, desc['setname']) + '_' + str(try_cnt)):
+                        if os.path.exists(os.path.join(self.inactive_folder_path, desc['setname']) + '_' + str(try_cnt)):
                             try_cnt += 1
                         else:
-                            dst_setname = os.path.join(self.inactive_files_path, desc['setname']) + '_' + str(try_cnt)
+                            dst_setname = os.path.join(self.inactive_folder_path, desc['setname']) + '_' + str(try_cnt)
                             break
                 else:
-                    dst_setname = os.path.join(self.inactive_files_path, desc['setname'])
+                    dst_setname = os.path.join(self.inactive_folder_path, desc['setname'])
                 # move the set
-                result = shutil.move(os.path.join(self.footage_path, desc['setname']), dst_setname)
+                result = shutil.move(os.path.join(self.active_folder_path, desc['setname']), dst_setname)
                 # remove from descriptions if 'move' was successful.
                 if os.path.exists(result):
-                    for i, description in enumerate(self.descriptions):
+                    for i, description in enumerate(self.active_descriptions):
                         if desc['setname'] == description['setname']:
-                            del self.descriptions[i]
+                            del self.active_descriptions[i]
         # update json file
         self.update_json()
 
+    def add_set(self, set_path):
+        # create a deprived description of the new set.
+        max_id = 0
+        for desc in all_descriptions:
+            if max_id < desc['id']:
+                max_id = desc['id']
+        new_desc = {
+            'id':max_id + 1,
+            'method':'',
+            'setup':'',
+            'subject':'',
+            'task':'',
+            'date':'',
+            'contents':'',
+            'labels':'',
+            'setname':os.path.basename(set_path),
+            'status':'active',
+        }
+        # add new_desc to both all_descriptions and active_descriptions
 
-        # for set_id in set_ids:
-        #     # move the folder to the inactive files folder.
-        #     # determine destination set name
-        #     dst_setname = ''
-        #     if os.path.exists(os.path.join(self.inactive_files_path, d_set['setname'])): # if folder already exists
-        #         try_cnt = 1
-        #         while(True):
-        #             if os.path.exists(os.path.join(self.inactive_files_path, d_set['setname']) + '_' + str(try_cnt)):
-        #                 try_cnt += 1
-        #             else:
-        #                 dst_setname = os.path.join(self.inactive_files_path, d_set['setname']) + '_' + str(try_cnt)
-        #                 break
-        #     else:
-        #         dst_setname = os.path.join(self.inactive_files_path, d_set['setname'])
-        #     # move the folder
-        #     result = shutil.move(os.path.join(self.footage_path, d_set['setname']), dst_setname)
-        #     # remove from descriptions if 'move' was successful.
-        #     if os.path.exists(result):
-        #         for i, description in enumerate(self.descriptions):
-        #             if d_set['setname'] == description['setname']:
-        #                 del self.descriptions[i]
-        # # update json file
-        # self.update_json()
 
 class DatasetManager(QWidget):
     def __init__(self):
@@ -137,6 +143,9 @@ class DatasetManager(QWidget):
         deleteAction = QAction('Delete',self)
         deleteAction.triggered.connect(self.on_delete)
         fileMenu.addAction(deleteAction)
+        addAction = QAction('Add from candidates',self)
+        addAction.triggered.connect(self.on_add)
+        fileMenu.addAction(addAction)
  
         self.createTable()
 
@@ -153,7 +162,7 @@ class DatasetManager(QWidget):
         desc_keys = self.F.get_desc_key_list()
        # Create table
         self.tableWidget = QTableWidget()
-        self.tableWidget.setRowCount(len(self.F.descriptions))
+        self.tableWidget.setRowCount(len(self.F.active_descriptions))
         self.tableWidget.setColumnCount(len(desc_keys))
         # self.tableWidget.setItem(0,c_i,QTableWidgetItem(key))
         self.tableWidget.setHorizontalHeaderLabels(desc_keys)
@@ -165,9 +174,9 @@ class DatasetManager(QWidget):
         self.tableWidget.horizontalHeader().sectionClicked.connect(self.on_header_click)
 
     def update_cells(self):
-        self.tableWidget.setRowCount(len(self.F.descriptions))
+        self.tableWidget.setRowCount(len(self.F.active_descriptions))
         desc_keys = self.F.get_desc_key_list()
-        for r_i, desc in enumerate(self.F.descriptions):
+        for r_i, desc in enumerate(self.F.active_descriptions):
             for c_i, key in enumerate(desc_keys):
                 # fill in string contents
                 if key in desc:
@@ -191,7 +200,7 @@ class DatasetManager(QWidget):
     @pyqtSlot()
     def on_cell_click(self):
         for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            selected_set_id = self.F.descriptions[currentQTableWidgetItem.row()]['id']
+            selected_set_id = self.F.active_descriptions[currentQTableWidgetItem.row()]['id']
             if selected_set_id in self.highlighted_set_ids:
                 self.highlighted_set_ids.remove(selected_set_id)
             else:
@@ -202,15 +211,24 @@ class DatasetManager(QWidget):
     @pyqtSlot()
     def on_header_click(self):
         c_i = self.tableWidget.selectedItems()[0].column()
-        # sort descriptions with respect to the selected key
-        self.F.sort_descriptions(self.F.get_desc_key_list()[c_i])
-        # update the table so that descriptions are ordered with the new result
+        # sort active_descriptions with respect to the selected key
+        self.F.active_descriptions = sorted(self.F.active_descriptions, key = lambda x:x[self.F.get_desc_key_list()[c_i]])
+        # update the table so that active_descriptions are ordered with the new result
         self.update_cells()
 
     @pyqtSlot()
     def on_delete(self):
         # delete highlighted files
-        self.F.delete_set(self.highlighted_set_ids)
+        self.F.delete_sets(self.highlighted_set_ids)
+        # update the table
+        self.update_cells()
+
+    @pyqtSlot()
+    def on_add(self):
+        # open file dialog and receive file list
+        selected_folder = QFileDialog.getExistingDirectory(self, "Select sets to be added.", self.F.candidate_folder_path) # one set at a time for now.
+        # make the deprived version of description for the new set. For now the user is responsible to modify descriptions after the set is added.
+        self.F.add_set(selected_folder)
         # update the table
         self.update_cells()
 
