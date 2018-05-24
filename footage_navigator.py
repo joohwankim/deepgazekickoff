44e7dee1544336d474a4cd614b1ecfc12a49e9b0
joohwankim <sckim@nvidia.com>
2018-05-23 17:20:03 -0700

    now we use pandas to manage the datasets, which makes data handling much cleaner. adding cell edit functionality.

--- footage_navigator.py
+++ footage_navigator.py
@@ -4,6 +4,7 @@
 # 3. Provide a clipboard copy of the paths to the selected items.
 
 import json, pdb, glob, os, sys, shutil
+import pandas as pd
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout,QHeaderView, QAbstractItemView, QMenuBar, QAction, QFileDialog, QInputDialog
 from PyQt5 import QtGui
 from PyQt5.QtGui import QClipboard
@@ -12,29 +13,21 @@ from PyQt5.QtCore import pyqtSlot
 
 class data_manager_backend():
     def update_json(self):
-        with open(os.path.join(self.footage_path, self.description_filename),'w') as fw:
-            json.dump(self.active_descriptions, fw, indent = 4)
+        with open(self.description_file_path,'w') as fw:
+            json.dump(self.descriptions.to_dict('records'), fw, indent = 4)
 
     def get_desc_key_list(self):
-        # generate key list of all the items in the descriptions
-        keys = set()
-        for desc in self.active_descriptions:
-            for key in desc.keys():
-                keys.add(key)
-        keys = list(keys)
+        # generate key list of all the name of variables in the descriptions
+        keys = list(self.descriptions)
         # Ordering of keys. We want to have the following keys to be at front.
         keys.sort()
-        predetermined_key_order = ['method','setup','subject','labels','contents']
+        predetermined_key_order = ['id','date','method','setup','subject','labels','contents']
         for k_i, key in enumerate(predetermined_key_order):
             if key in keys:
                 keys.insert(k_i, keys.pop(keys.index(key)))
         # Put setname at the end because it is long.
         keys.insert(len(keys), keys.pop(keys.index('setname')))
         return list(keys)
-
-    def sort_descriptions(self, sort_key): # sort active descriptions
-        sorted_descriptions = sorted(self.active_descriptions, key = lambda x:x[sort_key])
-        self.active_descriptions = sorted_descriptions
 
     def __init__(self, folder_path):
         # import descriptions
@@ -45,14 +38,7 @@ class data_manager_backend():
         self.description_file_path = os.path.join(folder_path, "descriptions.json")
         with open(self.description_file_path,'r') as fr:
             json_str = fr.read()
-            self.all_descriptions = json.loads(json_str)
-            self.active_descriptions = list()
-            self.inactive_descriptions = list()
-            for desc in self.all_descriptions:
-                if desc['status'] == 'active':
-                    self.active_descriptions.append(desc)
-                elif desc['status'] == 'inactive':
-                    self.inactive_descriptions.append(desc)
+            self.descriptions = pd.DataFrame(json.loads(json_str))
         if self.is_any_set_missing(): # make sure all the sets are present in the footage folder.
             sys.exit("Some sets listed in the description file are missing.")
 
@@ -60,12 +46,13 @@ class data_manager_backend():
         active_sets = os.listdir(self.active_folder_path)
         inactive_sets = os.listdir(self.inactive_folder_path)
         missing_set_count = 0
-        for desc in self.active_descriptions:
-            if not desc['setname'] in active_sets:
-                missing_set_count += 1
-        for desc in self.inactive_descriptions:
-            if not desc['setname'] in inactive_sets:
-                missing_set_count += 1
+        for idx, desc in self.descriptions.iterrows():
+            if desc['status'] == 'active':
+                if not desc['setname'] in active_sets:
+                    missing_set_count += 1
+            elif desc['status'] == 'inactive':
+                if not desc['setname'] in inactive_sets:
+                    missing_set_count += 1
         if missing_set_count > 0:
             return True
         else:
@@ -75,7 +62,7 @@ class data_manager_backend():
         # make sure inactive files folder exists and create one if not.
         if not os.path.exists(self.inactive_folder_path):
             os.mkdir(self.inactive_folder_path)
-        for desc in self.active_descriptions:
+        for idx, desc in self.descriptions[self.descriptions['status'] == 'active'].iterrows():
             if desc['id'] in set_ids: # this set should be deleted (moved to the 'inactive files' folder).
                 # determine the destination file name
                 dst_setname = ''
@@ -91,11 +78,9 @@ class data_manager_backend():
                     dst_setname = os.path.join(self.inactive_folder_path, desc['setname'])
                 # move the set
                 result = shutil.move(os.path.join(self.active_folder_path, desc['setname']), dst_setname)
-                # remove from descriptions if 'move' was successful.
+                # set status of the description to inactive if 'move' was successful.
                 if os.path.exists(result):
-                    for i, description in enumerate(self.active_descriptions):
-                        if desc['setname'] == description['setname']:
-                            del self.active_descriptions[i]
+                    self.descriptions.at[idx,'status'] = 'inactive'
         # update json file
         self.update_json()
 
@@ -119,6 +104,10 @@ class data_manager_backend():
         }
         # add new_desc to both all_descriptions and active_descriptions
 
+    def update_description(self, idx, key, value):
+        self.descriptions.at[idx,key] = value
+        self.update_json()
+
 
 class DatasetManager(QWidget):
     def __init__(self):
@@ -131,6 +120,7 @@ class DatasetManager(QWidget):
         self.w_width = 1200
         self.w_height = 800
         self.highlighted_set_ids = set()
+        self.sort_key = 'setname'
         self.initUI()
 
     def initUI(self):
@@ -162,7 +152,7 @@ class DatasetManager(QWidget):
         desc_keys = self.F.get_desc_key_list()
        # Create table
         self.tableWidget = QTableWidget()
-        self.tableWidget.setRowCount(len(self.F.active_descriptions))
+        self.tableWidget.setRowCount(len(self.F.descriptions[self.F.descriptions['status'] == 'active']))
         self.tableWidget.setColumnCount(len(desc_keys))
         # self.tableWidget.setItem(0,c_i,QTableWidgetItem(key))
         self.tableWidget.setHorizontalHeaderLabels(desc_keys)
@@ -171,21 +161,22 @@ class DatasetManager(QWidget):
         self.update_cells()
         # setting up call back functions
         self.tableWidget.cellClicked.connect(self.on_cell_click)
+        self.tableWidget.cellDoubleClicked.connect(self.on_cell_double_click)
         self.tableWidget.horizontalHeader().sectionClicked.connect(self.on_header_click)
 
     def update_cells(self):
-        self.tableWidget.setRowCount(len(self.F.active_descriptions))
+        # retrieve all the active descriptions from self.F, sort, and display.
+        self.tableWidget.setRowCount(len(self.F.descriptions[self.F.descriptions['status'] == 'active']))
         desc_keys = self.F.get_desc_key_list()
-        for r_i, desc in enumerate(self.F.active_descriptions):
+        # NOTE: displayed_descriptions is for making connections between user-selected cells and self.F.descriptions.
+        # Actual data management should directly happen on self.F.descriptions.
+        self.displayed_descriptions = self.F.descriptions[self.F.descriptions['status'] == 'active'].sort_values(by = self.sort_key).reset_index()
+        for r_i, desc in self.displayed_descriptions.iterrows():
             for c_i, key in enumerate(desc_keys):
                 # fill in string contents
                 if key in desc:
-                    content_string = ''
                     if type(desc[key]).__name__ == 'list':
-                        for i, item in enumerate(desc[key]):
-                            content_string += item
-                            if i < len(desc[key]) - 1:
-                                content_string += ', '
+                        content_string = ', '.join(desc[key])
                     elif type(desc[key]).__name__ != 'str':
                         content_string = str(desc[key])
                     else:
@@ -200,19 +191,43 @@ class DatasetManager(QWidget):
     @pyqtSlot()
     def on_cell_click(self):
         for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            selected_set_id = self.F.active_descriptions[currentQTableWidgetItem.row()]['id']
+            selected_set_id = self.displayed_descriptions.at[currentQTableWidgetItem.row(),'id']
             if selected_set_id in self.highlighted_set_ids:
                 self.highlighted_set_ids.remove(selected_set_id)
             else:
                 self.highlighted_set_ids.add(selected_set_id)
-        desc_keys = self.F.get_desc_key_list()
+        self.update_cells()
+
+    @pyqtSlot()
+    def on_cell_double_click(self): # edit a cell
+        displayed_i = self.tableWidget.selectedItems()[0].rows()
+        # find index in self.F.descriptions for the selected cell.
+        idx = self.F.descriptions.index[self.F.descriptions['id'] == self.displayed_descriptions.at[displayed_i, 'id']].tolist()[0]
+        key = self.F.get_desc_key_list()[self.tableWidget.selectedItems()[0].column()]
+        if key in ['method','setup','subject','task','setname','date']:
+            text, ok_pressed = QInputDialog.getText(self, 'Input text',key + ':', QLineEdit.Normal, self.F.descriptions.at[idx, key])
+            if ok_pressed:
+                self.F.update_description(idx,key,text)
+        elif key == 'setname':
+            text, ok_pressed = QInputDialog.getText(self, 'Input text',key + ':', QLineEdit.Normal, self.F.descriptions.at[idx, key])
+            if ok_pressed:
+                # attempt to change the folder name.
+                result = shutil.move(os.path.join(self.active_folder_path, desc['setname']), os.path.join(self.active_folder_path, text))
+                # if successful, change descriptions.
+                if os.path.basename(result) == text:
+                    self.F.update_description(idx,key,text)
+        elif key in ['labels','contents']:
+            content_string = ', '.join(self.F.descriptions.at[idx,key])
+            text, ok_pressed = QInputDialog.getText(self, 'Input text',key + ':', QLineEdit.Normal, content_string)
+            if ok_pressed:
+                content_list = text.replace(' ','').split(',')
+                self.F.update_description(idx,key,content_list)
         self.update_cells()
 
     @pyqtSlot()
     def on_header_click(self):
         c_i = self.tableWidget.selectedItems()[0].column()
-        # sort active_descriptions with respect to the selected key
-        self.F.active_descriptions = sorted(self.F.active_descriptions, key = lambda x:x[self.F.get_desc_key_list()[c_i]])
+        self.sort_key = self.F.get_desc_key_list()[c_i]
         # update the table so that active_descriptions are ordered with the new result
         self.update_cells()
 
